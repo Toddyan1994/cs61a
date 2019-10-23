c570f538a8950c604bb0852641e269afb0cd2ba7
toddy <yta11801@gmail.com>
2019-10-23 10:31:26 -0400

    hw 05 update

--- hw/hw05/hw05.py
+++ hw/hw05/hw05.py
@@ -510,10 +510,12 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[0]
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[1]
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -529,22 +531,24 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
+    return interval(lower_bound(x)-upper_bound(y),upper_bound(x)-lower_bound(y))
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert lower_bound(y)>0 or upper_bound(y)<0, "must not span zero"
     reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -566,8 +570,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1) # Replace this line!
-    r2 = interval(1, 1) # Replace this line!
+    r1 = interval(1, 2) # Replace this line!
+    r2 = interval(0.1, 0.2) # Replace this line!
     return r1, r2
 
 def multiple_references_explanation():
@@ -583,6 +587,16 @@ def quadratic(x, a, b, c):
     '0 to 10'
     """
     "*** YOUR CODE HERE ***"
+    def f(t,a,b,c):
+        return a*t*t+b*t+c
+    left=f(lower_bound(x),a,b,c)
+    right=f(upper_bound(x),a,b,c)
+    extreme_point=-b/(2*a)
+    if lower_bound(x)<extreme_point<upper_bound(x):
+        mid=f(extreme_point,a,b,c)
+        return interval(min(left,right,mid),max(left,right,mid))
+    else:
+        return interval(min(left,right),max(left,right))
 
 def polynomial(x, c):
     """Return the interval that is the range of the polynomial defined by
